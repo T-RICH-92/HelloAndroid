bc4524c253aab9a052033dc399c4f208c2ef7ff9
Tyson Richardson <trichar2@harding.edu>
2014-02-05 16:06:24 -0600

    Removed log statements

--- HelloAndroid/src/com/example/helloandroid/MainActivity.java
+++ HelloAndroid/src/com/example/helloandroid/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -11,9 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Log.v("tag", "This is a test");
-        Log.v("tag", "And another log statement");
     }
 
 
